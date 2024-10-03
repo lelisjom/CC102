32aafbc0ab5dc5e7f34057af370a13bc59f39677
lelisjom <jommarilelis08@gmail.com>
2024-10-03 10:02:06 +0800

    Add files via upload

---
+++ lelis_02basicoutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<< "School\t\t\t	:	Our Lady of Lourdes College Foundation, INC." << endl;
+	cout<< "Department\t\t\t:	OLLCF College of Computer Studies" << endl;
+	cout<< "Course & Year Level\t\t:	BSIT 1-A" << endl;
+	cout<< "Full Name\t\t	:	Jommari Lelis" << endl;
+	cout<< "Address\t\t\t	:	Magallanes Iraya P-6 Daet, Camarines Norte" << endl;
+	
+return 0;
+}
